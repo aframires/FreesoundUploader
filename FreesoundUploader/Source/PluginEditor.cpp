fb963b6bf2c8d392e38c88ae0ff43fcf7f43c776
aframires <aframires@gmail.com>
2019-01-11 17:42:30 +0100

    Initial commit

---
+++ FreesoundUploader/Source/PluginEditor.cpp
@@ -0,0 +1,84 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+FreesoundUploaderAudioProcessorEditor::FreesoundUploaderAudioProcessorEditor (FreesoundUploaderAudioProcessor& p)
+    :	AudioProcessorEditor (&p), processor (p),
+		state(Stopped),
+		thumbnailCache(5),
+		textToDisplay(std::make_shared<String>("Drop Audio Here!")),
+		thumbnailComp(512, formatManager, thumbnailCache,NULL),
+		positionOverlay(transportSource,NULL)
+{
+	addAndMakeVisible(&playButton);
+	playButton.setButtonText("Play");
+	playButton.onClick = [this] { playButtonClicked(); };
+	playButton.setColour(TextButton::buttonColourId, Colours::green);
+	playButton.setEnabled(false);
+	//e preciso por funcoes de listener
+
+	addAndMakeVisible(&stopButton);
+	stopButton.setButtonText("Stop");
+	stopButton.onClick = [this] { stopButtonClicked(); };
+	stopButton.setColour(TextButton::buttonColourId, Colours::red);
+	stopButton.setEnabled(false);
+
+	//textToDisplay = std::make_shared<String>("Drop Audio Here!");
+	//thumbnailComp = new SimpleThumbnailComponent(512, formatManager, thumbnailCache, textToDisplay);
+	//positionOverlay = new SimplePositionOverlay(transportSource, textToDisplay);
+
+	addAndMakeVisible(&thumbnailComp);
+	addAndMakeVisible(&positionOverlay);
+
+    setSize (600, 400);
+
+	formatManager.registerBasicFormats();
+	transportSource.addChangeListener(this);
+
+
+
+
+	//setAudioChannels(2, 2);
+}
+
+FreesoundUploaderAudioProcessorEditor::~FreesoundUploaderAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void FreesoundUploaderAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+}
+
+void FreesoundUploaderAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+
+	playButton.setBounds(10, 40, getWidth() - 20, 20);
+	stopButton.setBounds(10, 70, getWidth() - 20, 20);
+
+	Rectangle<int> thumbnailBounds(10, 100, getWidth() - 20, getHeight() - 120);
+	thumbnailComp.setBounds(thumbnailBounds);
+	positionOverlay.setBounds(thumbnailBounds);
+}
+
+void FreesoundUploaderAudioProcessorEditor::changeListenerCallback(ChangeBroadcaster* source)
+{
+	if (source == &transportSource)
+		transportSourceChanged();
+}
---
+++ FreesoundUploader/Source/PluginEditor.h
@@ -0,0 +1,284 @@
+/*
+  ==============================================================================
+
+	This file was auto-generated!
+
+	It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+
+class SimpleThumbnailComponent : public Component,
+	private ChangeListener
+{
+public:
+	SimpleThumbnailComponent(int sourceSamplesPerThumbnailSample,
+		AudioFormatManager& formatManager,
+		AudioThumbnailCache& cache,
+		std::shared_ptr<String> incomingTextToDisplay)
+		:	thumbnail(sourceSamplesPerThumbnailSample, formatManager, cache)
+			//textToDisplay(incomingTextToDisplay)
+	{
+		textToDisplay = NULL;
+		thumbnail.addChangeListener(this);
+	}
+
+	void setFile(const File& file)
+	{
+		thumbnail.setSource(new FileInputSource(file));
+	}
+
+	void paint(Graphics& g) override
+	{
+		if (thumbnail.getNumChannels() == 0)
+			paintIfNoFileLoaded(g);
+		else
+			paintIfFileLoaded(g);
+	}
+
+	void paintIfNoFileLoaded(Graphics& g)
+	{
+		g.fillAll(Colours::white);
+		g.setColour(Colours::darkgrey);
+		if(textToDisplay != NULL)
+		{
+		g.drawFittedText(*textToDisplay, getLocalBounds(), Justification::centred, 1.0f);
+		}
+	}
+
+	void paintIfFileLoaded(Graphics& g)
+	{
+		g.fillAll(Colours::white);
+
+		g.setColour(Colours::red);
+		thumbnail.drawChannels(g, getLocalBounds(), 0.0, thumbnail.getTotalLength(), 1.0f);
+
+	}
+
+	void changeListenerCallback(ChangeBroadcaster* source) override
+	{
+		if (source == &thumbnail)
+			thumbnailChanged();
+	}
+
+
+private:
+	void thumbnailChanged()
+	{
+		repaint();
+	}
+
+	AudioThumbnail thumbnail;
+	std::shared_ptr<String> textToDisplay;
+
+
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleThumbnailComponent)
+};
+
+class SimplePositionOverlay :	public Component,
+								private Timer,
+								public FileDragAndDropTarget
+
+{
+public:
+	SimplePositionOverlay(AudioTransportSource& transportSourceToUse, std::shared_ptr<String> textDisplayed)
+		:	transportSource(transportSourceToUse),
+			textEdit(textDisplayed)
+	{
+		//textEdit = textDisplayed;
+		startTimer(40);	
+	}
+
+	void paint(Graphics& g) override
+	{
+		auto duration = transportSource.getLengthInSeconds();
+		
+		if (somethingIsBeingDraggedOver)
+		{
+			g.setColour(Colours::red);
+			g.drawRect(getLocalBounds(), 3);
+		}
+
+		if (duration > 0.0)
+		{
+			auto audioPosition = transportSource.getCurrentPosition();
+			auto drawPosition = (audioPosition / duration) * getWidth();
+
+			g.setColour(Colours::green);
+			g.drawLine(drawPosition, 0.0f, drawPosition, (float)getHeight(), 2.0f);
+		}
+	}
+
+	void mouseDown(const MouseEvent& event) override
+	{
+		auto duration = transportSource.getLengthInSeconds();
+
+		if (duration > 0.0)
+		{
+			auto clickPosition = event.position.x;
+			auto audioPosition = (clickPosition / getWidth()) * duration;
+
+			transportSource.setPosition(audioPosition);
+		}
+	}
+
+	bool isInterestedInFileDrag(const StringArray& files) override
+	{
+
+		if(files.size() == 1){
+			//Confirm here that the type of file is the desired one. 
+			//See how to send the error messages to the thumbnail viewer
+			return true;
+		}
+		return false;
+	}
+
+	void fileDragEnter(const StringArray& /*files*/, int /*x*/, int /*y*/) override
+	{
+		somethingIsBeingDraggedOver = true;
+		repaint();
+	}
+
+	void fileDragMove(const StringArray& /*files*/, int /*x*/, int /*y*/) override
+	{
+	}
+
+	void fileDragExit(const StringArray& /*files*/) override
+	{
+		somethingIsBeingDraggedOver = false;
+		repaint();
+	}
+
+	void filesDropped(const StringArray& files, int /*x*/, int /*y*/) override
+	{
+
+		File droppedFile = files[0];
+		*textEdit = files[0];
+
+		somethingIsBeingDraggedOver = false;
+		repaint();
+	}
+
+private:
+	void timerCallback() override
+	{
+		repaint();
+	}
+
+	AudioTransportSource& transportSource;
+	bool somethingIsBeingDraggedOver = false;
+	std::shared_ptr<String> textEdit;
+
+
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimplePositionOverlay)
+};
+
+class FreesoundUploaderAudioProcessorEditor : public AudioProcessorEditor,
+	public ChangeListener
+{
+public:
+	FreesoundUploaderAudioProcessorEditor(FreesoundUploaderAudioProcessor&);
+	~FreesoundUploaderAudioProcessorEditor();
+
+	//==============================================================================
+	void paint(Graphics&) override;
+	void resized() override;
+	void changeListenerCallback(ChangeBroadcaster*) override;
+
+
+
+private:
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+
+
+	enum TransportState
+	{
+		Stopped,
+		Starting,
+		Playing,
+		Stopping
+	};
+
+	void changeState(TransportState newState)
+	{
+		if (state != newState)
+		{
+			state = newState;
+
+			switch (state)
+			{
+			case Stopped:
+				stopButton.setEnabled(false);
+				playButton.setEnabled(true);
+				transportSource.setPosition(0.0);
+				break;
+
+			case Starting:
+				playButton.setEnabled(false);
+				transportSource.start();
+				break;
+
+			case Playing:
+				stopButton.setEnabled(true);
+				break;
+
+			case Stopping:
+				transportSource.stop();
+				break;
+
+			default:
+				jassertfalse;
+				break;
+			}
+		}
+	}
+
+	void transportSourceChanged()
+	{
+		if (transportSource.isPlaying())
+			changeState(Playing);
+		else
+			changeState(Stopped);
+	}
+
+	void playButtonClicked()
+	{
+		changeState(Starting);
+	}
+
+	void stopButtonClicked()
+	{
+		changeState(Stopping);
+	}
+
+
+
+	FreesoundUploaderAudioProcessor& processor;
+
+
+	TextButton playButton;
+	TextButton stopButton;
+
+	AudioFormatManager formatManager;
+	std::unique_ptr<AudioFormatReaderSource> readerSource;
+	AudioTransportSource transportSource;
+	TransportState state;
+	AudioThumbnailCache thumbnailCache;
+	SimpleThumbnailComponent thumbnailComp;
+	SimplePositionOverlay positionOverlay;
+	std::shared_ptr<String> textToDisplay;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FreesoundUploaderAudioProcessorEditor)
+};
