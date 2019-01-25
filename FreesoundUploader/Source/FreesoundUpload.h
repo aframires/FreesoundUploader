7fdc6a6c3aa909512dbb51c9a2210ceba6768359
aframires <aframires@gmail.com>
2019-01-25 18:44:06 +0100

    Start work on Upload

---
+++ FreesoundUploader/Source/FreesoundUpload.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    FreesoundUpload.h
+    Created: 25 Jan 2019 6:33:51pm
+    Author:  Antonio
+
+  ==============================================================================
+*/
+
+#pragma once
+
+
+//API KEY/ClientID = at3tn6ciZuqgbjJUUvIQUVvAnPlJ4ynZbdj3YIjN
+
+//Login: OPEN IN BROWSER: https://freesound.org/apiv2/oauth2/authorize/?client_id=at3tn6ciZuqgbjJUUvIQUVvAnPlJ4ynZbdj3YIjN&response_type=code&state=xyz
+
+//Logout: 
